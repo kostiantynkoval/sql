0a5a8e988531c8a980d344bd79c6c8ace3721980
Kostiantyn Koval <kostiantynkoval@users.noreply.github.com>
2024-05-25 00:22:23 -0400

    Homework 5 is completed

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -30,19 +30,27 @@ INNER JOIN vendor AS v ON v.vendor_id = x.vendor_id
 This table will contain only products where the `product_qty_type = 'unit'`. 
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
-
+drop TABLE if EXISTS product_units;
+CREATE TABLE product_units as
+SELECT *, CURRENT_TIMESTAMP as snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
-
+INSERT INTO product_units 
+(product_id, product_name, product_size, product_category_id, product_qty_type, snapshot_timestamp) VALUES
+(999, 'Apple Pie volume 2', '9.5"', 3, 'unit', CURRENT_TIMESTAMP);
 
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
-
+DELETE FROM product_units WHERE snapshot_timestamp = (
+	SELECT max(snapshot_timestamp) FROM product_units
+);
 
 
 -- UPDATE
@@ -61,5 +69,15 @@ Third, SET current_quantity = (...your select statement...), remembering that WH
 Finally, make sure you have a WHERE statement to update the right row, 
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
-
-
+UPDATE product_units
+SET current_quantity = (
+	SELECT latest_inventory.quantity
+	FROM (
+		SELECT 
+			product_id, 
+			quantity,
+			ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY market_date DESC) AS rn
+		FROM vendor_inventory
+	) as latest_inventory
+	WHERE rn = 1 AND product_units.product_id = latest_inventory.product_id
+);
